06a8e6e9e5c1c8aa008013fcb41cd07fc4884eee
Khalish Madani <93044975+KhalishMadani@users.noreply.github.com>
2026-05-10 11:06:44 +0800

    feat: musafir xcode

---
+++ Musafir-Xcode/Musafir_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Musafir_XcodeApp.swift
+//  Musafir-Xcode
+//
+//  Created by Muhammad Khalish Madani on 10/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Musafir_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
